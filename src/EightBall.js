22249e39a7a1c77bead340e8834c3e1fa66bde14
alexm <alex-m@outlook.com>
2023-07-14 12:15:41 -0700

    Set up and export basic EightBall component

---
+++ src/EightBall.js
@@ -0,0 +1,17 @@
+// Ioana A Mititean
+// 37.4: React State - Eight Ball
+
+import React from 'react';
+
+
+function EightBall(props) {
+
+    return (
+        <div>
+
+        </div>
+    )
+}
+
+
+export default EightBall;
