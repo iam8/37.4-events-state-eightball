c780d2abb838740ebe675c1785a289f66cab9ede
alexm <alex-m@outlook.com>
2023-07-14 13:02:49 -0700

    Render Eight ball with color and message; write event handler for updating eight ball display when clicked

--- src/EightBall.js
+++ src/EightBall.js
@@ -3,17 +3,34 @@
 
 import React from 'react';
 import { useState } from 'react';
+import "./EightBall.css";
 
 
 function EightBall({answers}) {
 
-    // Set initial state
     const [msg, setMsg] = useState("Think of a Question");
     const [color, setColor] = useState("black");
 
+    /**
+     * Update Eight Ball with a randomly-chosen message and color.
+     */
+    function updateBallDisplay() {
+
+        const idx = Math.floor(Math.random() * answers.length);
+        const answer = answers[idx];
+
+        setMsg(answer.msg);
+        setColor(answer.color);
+    }
+
     return (
         <div className="EightBall">
+            <h4>Current message: {msg}</h4>
+            <h4>Current color: {color}</h4>
 
+            <div className="EightBall-ball" onClick={updateBallDisplay}>
+                <div className="EightBall-msg">{msg}</div>
+            </div>
         </div>
     )
 }
